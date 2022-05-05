da8b2ff1099e88357b018f9fbb8ac2e8284ada37
dbrcina <dbrcina@donotreply.github>
2022-05-05 13:46:37 +0200

    wip

--- src/models.py
+++ src/models.py
@@ -15,25 +15,25 @@ class LeNet5(nn.Module):
         # In the paper, 32x32 image is expected, but here 28x28 is given, so padding is set to 2
         self.features = nn.Sequential(
             # 1
-            nn.Conv2d(1, 6, kernel_size=5, padding=2),  # Nx6x28x28
+            nn.Conv2d(1, 16, kernel_size=5, padding=2),  # Nx16x28x28
             act_fun,
-            nn.MaxPool2d(kernel_size=2, stride=2),  # Nx6x14x14
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Nx16x14x14
             # 2
-            nn.Conv2d(6, 16, kernel_size=5),  # Nx16x10x10
+            nn.Conv2d(16, 32, kernel_size=5),  # Nx32x10x10
             act_fun,
-            nn.MaxPool2d(kernel_size=2, stride=2),  # Nx16x5x5
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Nx32x5x5
             # 3
-            nn.Conv2d(16, 120, kernel_size=5),  # Nx120x1x1
+            nn.Conv2d(32, 64, kernel_size=5),  # Nx64x1x1
             act_fun,
-            nn.Flatten()  # Nx120 (120*1*1)
+            nn.Flatten()  # Nx64 (64*1*1)
         )
         self.classifier = nn.Sequential(
             # 4
-            nn.Linear(120, 84),  # Nx84
+            nn.Linear(64, 128),  # Nx128
             act_fun,
             # 5
             drop_fun,
-            nn.Linear(84, n_classes)  # Nxn_classes
+            nn.Linear(128, n_classes)  # Nxn_classes
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -57,33 +57,33 @@ class AlexNet(nn.Module):
 
         self.features = nn.Sequential(
             # 1
-            nn.Conv2d(3, 32, kernel_size=11, stride=4),  # Nx32x55x55
+            nn.Conv2d(3, 16, kernel_size=11, stride=4),  # Nx16x55x55
             act_fun,
-            nn.MaxPool2d(kernel_size=3, stride=2),  # Nx32x27x27
+            nn.MaxPool2d(kernel_size=3, stride=2),  # Nx16x27x27
             # 2
-            nn.Conv2d(32, 64, kernel_size=5, padding=2),  # Nx64x27x27
+            nn.Conv2d(16, 32, kernel_size=5, padding=2),  # Nx32x27x27
             act_fun,
-            nn.MaxPool2d(kernel_size=3, stride=2),  # Nx64x13x13
+            nn.MaxPool2d(kernel_size=3, stride=2),  # Nx32x13x13
             # 3
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),  # Nx128x13x13
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),  # Nx64x13x13
             act_fun,
             # 4
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),  # Nx128x13x13
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),  # Nx32x13x13
             act_fun,
             # 5
-            nn.Conv2d(128, 64, kernel_size=3, padding=1),  # Nx64x13x13
+            nn.Conv2d(32, 16, kernel_size=3, padding=1),  # Nx16x13x13
             act_fun,
-            nn.MaxPool2d(kernel_size=3, stride=2),  # Nx64x6x6
-            nn.Flatten()  # Nx2304 (64*6*6)
+            nn.MaxPool2d(kernel_size=3, stride=2),  # Nx16x6x6
+            nn.Flatten()  # Nx576 (16*6*6)
         )
         self.classifier = nn.Sequential(
             # 6
             drop_fun,
-            nn.Linear(2304, 1024),  # Nx1024
+            nn.Linear(576, 512),  # Nx512
             act_fun,
             # 7
             drop_fun,
-            nn.Linear(1024, 256),  # Nx256
+            nn.Linear(512, 256),  # Nx256
             act_fun,
             # 8
             nn.Linear(256, n_classes)  # Nxn_classes
